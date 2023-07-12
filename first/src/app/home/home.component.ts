fb47057497ec1dca15868b9ff8fb30a0e4a9c70c
EllipticEllipsis <elliptic.ellipsis@gmail.com>
2023-07-12 03:12:01 +0100

    Add filter guard to data refresh

--- first/src/app/home/home.component.ts
+++ first/src/app/home/home.component.ts
@@ -15,27 +15,39 @@ export class HomeComponent {
   questionsService: QuestionsService = inject(QuestionsService);
   questionList: Question[] = [];
   filteredQuestionList: Question[] = [];
+  filtered: boolean = false;
   topicsList: string[] = this.questionsService.getAllTopics();
-  
+
   constructor() {
-    this.questionsService.questionsEmitter.subscribe(questions => this.questionList = questions);
-    this.filteredQuestionList = this.questionList;
+    this.questionsService.questionsEmitter.subscribe(
+      questions => {
+        this.questionList = questions;
+        // Only update when no filter applied
+        if (!this.filtered) {
+          this.filteredQuestionList = this.questionList;
+        }
+      });
     console.log(this.questionList);
   }
-  
+
+  // TODO: topic filters
   filterResults(year: string, section: string, course: string) {
     this.filteredQuestionList = this.questionList;
+    this.filtered = false;
     if (year) {
       this.filteredQuestionList =
-        this.filteredQuestionList.filter(question => question?.year === Number(year))
+        this.filteredQuestionList.filter(question => question?.year === Number(year));
+      this.filtered = true;
     }
     if (section) {
       this.filteredQuestionList =
         this.filteredQuestionList.filter(question => question?.section === section as Section)
+      this.filtered = true;
     }
     if (course) {
       this.filteredQuestionList =
         this.filteredQuestionList.filter(question => question?.course === course)
+      this.filtered = true;
     }
   }
 
